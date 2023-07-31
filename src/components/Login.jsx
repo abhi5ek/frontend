8f3dd69f75a4d4ff8174f0604de0537d5ed25376
Abhishek Kumar Pandey <abhishekpandey7676@gmail.com>
2023-08-01 02:05:06 +0530

    chnages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div className='card col-md-3 bg-primary text-white h-100px d-flex justify-content-center'>
+    <div className='card-header'>
+      <h1 style={{textAlign:"center" , font:"bold"}}>LOGIN</h1>
+    </div>
+    <div className='card-body w-100 p-2 display-box h-50vh font-bold text-center'>
+      <label className='text-white p-10' htmlFor="Email"> Email </label> <br />
+      <input type="email" />
+
+      <br /> <br />
+
+      <label htmlFor="Password"> Password </label> <br />
+      <input type="password" />
+
+
+      <br /><br />
+
+      <button className='mybtn bg-transparent text-white rounded-3'>LOGIN</button>
+
+    </div>
+  </div>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-        <h1>Signup Page</h1>
+    <div className='card col-md-4 bg-info text-white h-100px d-flex justify-content-center'>
+      <div className='card-header'>
+        <h1 style={{textAlign:"center" , font:"bold"}}>SIGNUP</h1>
+      </div>
+      <div className='card-body w-100 p-2 display-box h-50vh font-bold text-center'>
+        <label className='text-white p-10' htmlFor="Email"> Email </label> <br />
+        <input type="email" />
+
+        <br /> <br />
+
+        <label htmlFor="Password"> Password </label> <br />
+        <input type="text" />
+
+        <br /><br />
+
+        <label htmlFor="Password"> Confirm Password </label> <br />
+        <input type="password" />
+
+        <br /><br />
+
+        <button className='mybtn bg-transparent text-white rounded-3'>SIGNUP</button>
+
+      </div>
     </div>
   )
 }
