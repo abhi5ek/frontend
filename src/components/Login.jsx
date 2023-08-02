a4493a8b4a1e95aaa7843d7b63b440e2eed3eeaa
Abhishek Kumar Pandey <abhishekpandey7676@gmail.com>
2023-08-03 01:49:16 +0530

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,26 +2,36 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='card col-md-3 bg-primary text-white h-100px d-flex justify-content-center'>
-    <div className='card-header'>
-      <h1 style={{textAlign:"center" , font:"bold"}}>LOGIN</h1>
+    <div className='container d-flex justify-content-center mt-5'>
+
+      <div className='card bg-primary d-flex align-items-center col-md-4 text-white fw-bold'>
+           
+           <div className='card-header'>
+
+             <h1 style={{alignItems:"center"}}>Login</h1> 
+
+           </div>
+
+        <div >
+
+          <label htmlFor="email">Email</label>  <br />
+          <input type="email" />
+
+          <br /><br />
+          
+          
+          <label htmlFor="password">Password</label>  <br />
+          <input type="password" />
+
+          <br /><br />
+
+          <button className='mybtn btn-success align-items-center mb-4'>Login</button>
+
+        </div>
+
+      </div>
+
     </div>
-    <div className='card-body w-100 p-2 display-box h-50vh font-bold text-center'>
-      <label className='text-white p-10' htmlFor="Email"> Email </label> <br />
-      <input type="email" />
-
-      <br /> <br />
-
-      <label htmlFor="Password"> Password </label> <br />
-      <input type="password" />
-
-
-      <br /><br />
-
-      <button className='mybtn bg-transparent text-white rounded-3'>LOGIN</button>
-
-    </div>
-  </div>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div className='container d-flex justify-content-center mt-5 '>
-    <div className='card col-md-4 bg-info text-white h-100px d-flex justify-content-center'>
+    <div className='container d-flex justify-content-center mt-5 text-white '>
+    <div className='card col-md-4 fw-bold bg-info text-white'>
       <div className='card-header'>
-        <h1 style={{textAlign:"center" , font:"bold"}}>SIGNUP</h1>
+        <h1 className='text-center'>SIGNUP</h1>
       </div>
-      <div className='card-body w-100 p-2 display-box h-50vh fw-bold text-center'>
-        <label className='text-white p-10' htmlFor="Email"> Email </label> <br />
+      <div className='card-body fw-bold text-center'>
+        <label htmlFor="Email"> Email </label> <br />
         <input type="email" />
 
         <br /> <br />
