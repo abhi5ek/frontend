e42621c69e01e13a52524280a72ecc8866ed973a
Abhishek Kumar Pandey <abhishekpandey7676@gmail.com>
2023-08-06 14:05:29 +0530

    changes

--- src/components/ListProduct.jsx
+++ src/components/ListProduct.jsx
@@ -4,30 +4,107 @@ import smartphones from './dummyData';
 const ListProduct = () => {
     const [productData, setProductData] = useState(smartphones);
 
+    const brands = [ 'Samsung' , 'OnePlus' , 'Apple' , 'MotoRola' ];
+
+    const model = [ 'Galaxy S23' , 'Iphone 14' , 'OnePlus 11R' , 'g14' ,'g62' , 'Galaxy A54'];
+
     const displayProducts = () => {
         return productData.map((phone) => {
-            return <div className='col-md-3'>
+            return <div className='col-md-3 mt-4'>
                 <div className='card'>
-                    <img className='card-img-top' src={phone.image} alt="Not Available" />
+                    <div className='m-auto h-50px'>
+                    <img style={{height:'200px' , objectFit:'cover'}} className='card-img-top' src={phone.image} alt="Not Available" />
+                    </div>
                     <div className='card-body'>
                         <h5>{phone.brand}</h5>
                         <h3>{phone.model}</h3>
                         <h2>₹{phone.price}</h2>
+                        <button className='btn btn-primary'>Buy</button>
+                        <button className='btn btn-secondary ms-3'>Cart</button>
                     </div>
                 </div>
             </div>
-        })
+        });
+    };
+
+
+    const searchProduct = (e) => {
+        const search = e.target.value;
+        setProductData(
+            smartphones.filter( (phone) => {  return phone.model.toLowerCase().includes(search.toLowerCase()) || phone.brand.toLowerCase().includes(search.toLowerCase())})
+        )
+    }
+
+    const filterBrands = (e) => {
+        const search = e.target.value;
+        setProductData(
+            smartphones.filter((phone) => {
+                return phone.brand === search;
+            })
+        );
+    };
+
+    const filtermodel = (e) => {
+        const search = e.target.value;
+        setProductData(
+            smartphones.filter((phone) => {
+                return phone.model === search;
+            })
+        )
     }
 
   return (
     <div>
-        <div className='container'>
-            <div className='row'>
-                {displayProducts()}
-            </div>
-        </div>
+
+        <header className='bg-secondary'>
+           <div className='container py-5'>
+             <h1 className='text-center'>Product List Page</h1>
+             <input onChange={searchProduct} type="text" className='form-control form-control-lg' placeholder='Search Product Name...'/>
+           </div>
+
+
+        </header>
+           <div className='row'>
+             <div className='col-2'>
+                <div className='card mt-4'>
+                    
+                    <div className='card-header'>
+                          <h3>Filters</h3>
+                    </div>
+                    
+                    <div className='card-body '>
+                          <label htmlFor="brand"><h4>By Brand</h4></label>
+                          <select onChange={filterBrands}>
+                          {brands.map((brand) => (
+                               <option value={brand}>{brand}</option>
+                          ))}
+                          </select>
+                          <label htmlFor="model"><h4>By Model</h4></label>
+                          <select onChange={filtermodel}>
+                          {model.map((model) => (
+                               <option value={model}>{model}</option>
+                          ))}
+                          </select>
+                    </div>
+                </div>
+             </div>
+             <div className='col'>
+                <div className='row'>
+                    {displayProducts()}
+                </div>
+             </div>
+             {/* <div className='col-2'>
+                <div className='card mt-4'>
+                    <div className='card-body'>
+                        <h3>abhishek</h3>
+                    </div>
+                </div>
+             </div> */}
+           </div> 
     </div>
   )
 }
 
 export default ListProduct
+
+
--- src/components/dummyData.js
+++ src/components/dummyData.js
@@ -2,26 +2,44 @@ const smartphones = [
     {
         brand : 'Samsung',
         model : 'Galaxy S23',
-        price : 8000,
+        price : 80000,
         image :'https://cdn1.smartprix.com/rx-iuwYeMkkW-w1200-h1200/uwYeMkkW.jpg'
     },
     {
-        brand : 'Samsung',
-        model : 'Galaxy S23',
-        price : 8000,
-        image : 'https://cdn1.smartprix.com/rx-iuwYeMkkW-w1200-h1200/uwYeMkkW.jpg'
+        brand : 'Apple',
+        model : 'Iphone 14',
+        price : 179999,
+        image : 'https://img5.gadgetsnow.com/gd/images/products/additional/large/G390872_View_1/mobiles/smartphones/apple-iphone-14-pro-max-1-tb-gold-6-gb-ram-.jpg'
+    },
+    {
+        brand : 'OnePlus',
+        model : 'OnePlus 11R',
+        price : 39999,
+        image : 'https://m.media-amazon.com/images/I/41g1pQr8xUL._SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+        brand : 'MotoRola',
+        model : 'g14',
+        price :  14999,
+        image : 'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/d/h/f/g13-pawv0028in-motorola-original-imagz4rbawxkbyjg.jpeg?q=70'
+    },
+    {
+        brand : 'MotoRola',
+        model : 'g62',
+        price :  17999,
+        image : 'https://www.reliancedigital.in/medias/Motorola-G62-Mobile-Phone-493177199-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w4NTIwOHxpbWFnZS9qcGVnfGltYWdlcy9oZjMvaGI0Lzk4NzMyNjY4NjgyNTQuanBnfDk1ZDNkMDkxMGE4Zjk3NjQzNGM1ZTFkOGNlNDJlMzZiOGQxNWY0ZTcyNzdlZTZkOTM4NDMzZjY0ODM1YTA3Y2E'
     },
     {
         brand : 'Samsung',
         model : 'Galaxy S23',
-        price : 8000,
-        image : 'https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/t/7/r/-original-imagmg6gyxbn4md9.jpeg?q=90'
+        price : 80000,
+        image : 'https://cdn1.smartprix.com/rx-iuwYeMkkW-w1200-h1200/uwYeMkkW.jpg'
     },
     {
         brand : 'Samsung',
-        model : 'Galaxy S23',
-        price : 8000,
-        image : 'https://cdn1.smartprix.com/rx-iuwYeMkkW-w1200-h1200/uwYeMkkW.jpg'
+        model : 'Galaxy A54',
+        price : 38999,
+        image :'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS51Y-QgYkZhCuvqWtPO2ar73GVR95dHJoeMiC0kS26JkVLsQZhqTXaQZ8qAj5hTdDU4O5Nlp0CM1FXhDoSzP_8r_yLO-WL5T43WK6VCmryNKdWk0VU5YDe&usqp=CAc'
     }
 ];
 
